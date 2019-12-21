1f7ecd8e3057f0a1868467b2c125b8aecb7148a5
Yajana N Rao <yajananrao@gmail.com>
2019-12-21 13:20:43 +0530

    Update LargestOfTwo.v

--- Function/LargestOfTwo.v
+++ Function/LargestOfTwo.v
@@ -1,13 +1,14 @@
 `timescale 1ns / 1ps
 // function to find the greater of two signed numbers
 module LargestOfTwo(g,enable,a,b);
-	input enable, a, b;
-	output reg g;
+	input enable;
+	input signed [2:0] a, b;
+	output reg [2:0] g;
 	always @(enable)
 		g = largest(a,b);
 		
-		function largest;
-			input a,b;
+		function [2:0] largest;
+			input [2:0] a,b;
 				begin
 					if(a>b)
 						largest=a;
