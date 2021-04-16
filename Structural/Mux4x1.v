953ff9840d538747de77f3a5185abce3bbceb4a4
Yajana N Rao <yajananrao@gmail.com>
2021-04-16 17:33:42 +0000

    Added counter

--- Structural/Mux4x1.v
+++ Structural/Mux4x1.v
@@ -1,14 +1,13 @@
 `timescale 1ns / 1ps
 
-module Mux4to1Using2to1(out,i0,i1,i2,i3,s1,s0);
+module Mux4x1(out,i0,i1,i2,i3,s1,s0);
 	input i0,i1,i2,i3,s1,s0;
 	output out;
 	wire mux1,mux2;
 	mux2x1 mux_1(mux1,i0,i1,s1);
 	mux2x1 mux_2(mux2,i2,i3,s1);
 	mux2x1 mux_3(out,mux1,mux2,s0);
-
-endmodule
+endmodule;
 
 
 module mux2x1(out,a,b,s);
@@ -19,4 +18,4 @@ module mux2x1(out,a,b,s);
 	and (and_1,a,s_c);
 	and (and_2,b,s);
 	or (out,and_1,and_2);
-endmodule
+endmodule;
---
+++ Structural/UpDownCounter.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+//Verilog module for UpDown counter
+
+module UpDownCounter(clk,mode, count);
+
+    input clk, mode;
+    output [3:0] count;
+    reg [3:0] count = 0;  
+
+    reg [24:0] clkdiv = 0;
+    
+    initial count = 0;
+
+    always @(posedge clkdiv[24])
+        begin    
+            if(mode == 1)   
+                //Up mode selected   
+                count = count + 1; 
+                //Incremend Counter
+            else  
+                //Down mode selected    
+                count = count - 1; 
+                //Decrement counter
+        end    
+    
+    always @(posedge clkdiv)
+        clkdiv = clkdiv + 1;
+
+
+endmodule
